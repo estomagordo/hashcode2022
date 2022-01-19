0792d1ca6772e26bce8918ad3830e3c0b7860d63
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2022-01-19 20:33:39 +0100

    preliminary version with brute force

---
+++ onepizza.py
@@ -0,0 +1,55 @@
+from itertools import combinations
+from sys import argv, stdin
+
+
+def get_input():
+    lines = stdin.readlines()
+    c = int(lines[0])
+    customers = []
+
+    for x in range(1, 2 * (c+1), 2):
+        likes = lines[x].split()[1:]
+        dislikes = lines[x+1].split()[1:]
+
+        customers.append((likes, dislikes))
+
+    return c, customers
+
+
+def get_all_ingredients(customers):
+    all_ingredients = set()
+
+    for like, dislike in customers:
+        all_ingredients |= set(like)
+        all_ingredients |= set(dislike)
+
+    return list(all_ingredients)
+
+
+def score_toppings(toppings, customers):
+    return sum(all(like in toppings for like in likes) and not any(dislike in toppings for dislike in dislikes) for likes, dislikes in customers)
+
+
+def solve_brute(c, customers):
+    all_ingredients = get_all_ingredients(customers)
+    best = (0, [])
+
+    for topping_count in range(len(all_ingredients)+1):
+        for toppings in combinations(all_ingredients, topping_count):
+            score = score_toppings(toppings, customers)
+            if score > best[0]:
+                best = (score, toppings)
+
+    return best[1]
+
+
+def write_results(solution, file_name):
+    with open(file_name, 'w') as f:
+        f.write(f'{len(solution)} {" ".join(solution)}')
+
+c, customers = get_input()
+solution = solve_brute(c, customers)
+input_file_name = argv[2]
+output_file_name = '.'.join(map(lambda part: 'out' if part == 'in' else part, input_file_name.split('.')))
+
+write_results(solution, output_file_name)
